af377791d56089c610beca48b93c38800e6ed170
dan <dan@gmail.com>
2024-08-29 23:57:41 +0800

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +18,7 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': resolve(__dirname, './src')
     }
   },
   base: '/', // 確保 base 路徑設為根目錄
