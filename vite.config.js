822ab4b608583f7ba2ec8b172882c5b7f1e26233
dan <dan@gmail.com>
2024-08-29 23:44:50 +0800

    vercel json

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  base: '/', // 確保 base 路徑設為根目錄
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
